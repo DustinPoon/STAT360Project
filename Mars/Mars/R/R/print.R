e97f3150674793637ca454246761f2b548a309aa
dpoon <97767547+DustinPoon@users.noreply.github.com>
2023-04-15 14:55:21 -0700

    Add files via upload

---
+++ Mars/Mars/R/R/print.R
@@ -0,0 +1,21 @@
+#' Print method for MARS Model
+#'
+#' @description Print coefficients of the fitted mars model.
+#'
+#' @param x MARS model object (input mars object)
+#' @param ... further arguments passed to or from other methods.
+#'
+#' @examples
+#' fit.mars <- mars(wage ~ age + education, data=ISLR::Wage, control = mars.control(Mmax=10))
+#' print.mars(fit.mars)
+#'
+#' @rdname print.mars
+#' @export print.mars
+#' @export
+print.mars <- function(x, ...){
+  cat("\n Call: \n")
+  print.default(x$call)
+
+  cat("\n Coefficients: \n")
+  print.default(x$coefficients)
+}
