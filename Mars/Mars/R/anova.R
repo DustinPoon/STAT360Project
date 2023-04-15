ed509f93c8bfd575f29678c2bfe630891adae834
tylero-sfu <58358359+tylero-sfu@users.noreply.github.com>
2023-04-15 00:18:28 -0700

    Changes anova and vignettes

--- Mars/Mars/R/anova.R
+++ Mars/Mars/R/anova.R
@@ -1,11 +1,17 @@
-#' Anova for MARS
+#' Anova method for MARS
 #'
-#' @param object An object of class mars.
-#' @param ... Other arguments.
-#' @return The anova table of the fitted MARS model.
+#' @description Compute an analysis of variance table for the fitted MARS model.
 #'
+#' @param object an object of class 'mars'.
+#' @param ... other arguments -- currently not used.
+#'
+#' @examples
+#' fit.mars <- mars(wage ~ age + education, data=ISLR::Wage, control = mars.control(Mmax=10))
+#' anova.mars(fit.mars)
+#'
+#' @rdname anova.mars
+#' @export anova.mars
 #' @export
-
 anova.mars <- function(object, ...) {
   anova(lm(y ~ ., data = data.frame(y = object$y, object$B)))
 }
